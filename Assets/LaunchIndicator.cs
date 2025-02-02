22d041efa46f874a8b1a512c9308157b678b87a9
Hoc Nguyen <144164385+Hocng7@users.noreply.github.com>
2025-02-01 21:44:53 -0800

    Launch Indicator

---
+++ Assets/LaunchIndicator.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using Unity.Cinemachine;
+
+
+public class ArrowIndicator : MonoBehaviour
+{
+    [SerializeField] private CinemachineCamera freeLookCamera;
+
+    private void Update()
+    {
+        transform.forward = freeLookCamera.transform.forward;
+        transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0);
+    }
+}
