670a0460bb06f44d8618b16b7734e09fbc99474e
Hoc Nguyen <144164385+Hocng7@users.noreply.github.com>
2025-02-01 17:51:02 -0800

    Gutters implementation

---
+++ Assets/Gutter.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Gutter : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider triggeredBody)
+    {
+        // get the rigid body of the ball
+        Rigidbody ballRigidBody = triggeredBody.GetComponent<Rigidbody>();
+
+        // store the velocity magnitude before resettting the velocity
+        float velocityMagnitude = ballRigidBody.linearVelocity.magnitude;
+
+        //reset both linear & angular velocity of the ball
+        ballRigidBody.linearVelocity = Vector3.zero;
+        ballRigidBody.angularVelocity = Vector3.zero;
+
+        // Adding force in the forward direction of the gutter
+        ballRigidBody.AddForce(transform.forward * velocityMagnitude, ForceMode.VelocityChange);
+
+    }
+}
